10e29bfc2f27c7836250f834d453c6d35c5b91c5
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-07-08 16:25:25 -0600

    deploy vercel

--- api/routes/products.router.js
+++ api/routes/products.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const ProductService = require('./../services/product.service');
-const validatorHandler = require('./../middlewares/validator.handler');
+const ProductService = require('../services/product.service');
+const validatorHandler = require('../middlewares/validator.handler');
 const {
   createProductSchema,
   updateProductSchema,
   getProductSchema,
-} = require('./../schemas/product.schema');
+} = require('../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductService();
